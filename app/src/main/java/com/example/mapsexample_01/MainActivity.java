c6027a4f89b763263f55473045af45bcd76a2d68
luispipe <lflondonor@unal.edu.co>
2023-12-12 08:14:34 -0500

    first commit

---
+++ app/src/main/java/com/example/mapsexample_01/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mapsexample_01;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnMap=findViewById(R.id.btnMap);
+        Intent maps= new Intent(getApplicationContext(), MapsActivity.class);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(maps);
+            }
+        });
+    }
+}
